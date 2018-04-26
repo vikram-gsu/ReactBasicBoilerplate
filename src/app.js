98df31ab4403d834fec36504480ae69a13fc4610
vikram-gsu <vpareddy1@student.gsu.edu>
2018-04-26 06:46:36 -0400

    Removed all the Indecision app specific files to create a basic boilerplate

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Indecision from './components/Indecision';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
-  ReactDOM.render(<Indecision />, document.getElementById('app'))
+  ReactDOM.render(<p>React-boilerplate</p>, document.getElementById('app'))
